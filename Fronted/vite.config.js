bd557822101d3fef526f837f0ad122ac840b7acc
Ashutosh1702 <ashucreater06@gmail.com>
2025-08-09 19:56:18 +0530

    Fix MIME type error and asset serving on Vercel

--- Fronted/vite.config.js
+++ Fronted/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
   server: {
     proxy: {
       '/api': {
@@ -21,6 +21,9 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: undefined,
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
       },
     },
   },
